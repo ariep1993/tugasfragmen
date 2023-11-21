eff37748525555c72445b8e7f7dddf0a07aaa406
saripudin <arieptea93@gmail.com>
2023-11-22 03:04:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugassplash/MapsActivity.java
@@ -0,0 +1,4 @@
+package com.example.tugassplash;
+
+public class MapsActivity {
+}
---
+++ app/src/main/java/com/example/tugassplash/sianidaActivity.java
@@ -0,0 +1,34 @@
+package com.example.tugassplash;
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+import android.os.Bundle;
+
+
+
+
+public class sianidaActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sianida);
+
+    }
+
+}
+
+
+
